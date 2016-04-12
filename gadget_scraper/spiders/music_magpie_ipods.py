1da9fa1805e2b4729d824eac35aee7c1a63e2f3a
Danil Bokov <a35012a@gmail.com>
2016-04-12 10:18:01 +0600

    Updated spider

--- gadget_scraper/spiders/music_magpie_ipods.py
+++ gadget_scraper/spiders/music_magpie_ipods.py
@@ -1,13 +1,43 @@
 # -*- coding: utf-8 -*-
-import scrapy
 
+from scrapy import Spider, Request, FormRequest
+from scrapy.selector import Selector
+import json
+try:
+	from items import GadgetScraperItem
+except ImportError:
+	from gadget_scraper.items import GadgetScraperItem
 
-class MusicMagpieIpodsSpider(scrapy.Spider):
-    name = "music_magpie_ipods"
-    allowed_domains = ["www.musicmagpie.co.uk"]
-    start_urls = (
-        'http://www.www.musicmagpie.co.uk/',
-    )
+class MusicMagpieIpodsSpider(Spider):
+	name = "music_magpie_ipods"
+	allowed_domains = ["musicmagpie.co.uk"]
+	start_urls = ("http://www.musicmagpie.co.uk",)
 
-    def parse(self, response):
-        pass
+	def parse(self, response):
+		formdata={
+			"category": "Model",
+			"knownCategoryValues": "undefined:iPod;Make:Apple;",
+		}
+		
+		request = Request(
+			url="http://www.musicmagpie.co.uk/usercontrols/techService.asmx/GetModels",
+			method='POST', 
+			body=json.dumps(formdata),			
+			headers={"Content-Type": "application/json; charset=UTF-8"},
+			meta={"make": "Apple"},
+			callback=self.parse_ipods_models,
+			)
+		
+		yield request
+		
+	def parse_ipods_models(self, response):
+		json_response = json.loads(response.body)
+		
+		for ipod_entry in json_response["d"]:
+			item = GadgetScraperItem()
+			model = ipod_entry["name"].strip()
+			
+			item["make"] = response.meta["make"]
+			item["model"] = model
+			
+			yield item
