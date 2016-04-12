61b26644cab05617f4f4539381860fae717cf7aa
Danil Bokov <a35012a@gmail.com>
2016-04-12 15:06:21 +0600

    Refactored and added new model

--- gadget_scraper/items.py
+++ gadget_scraper/items.py
@@ -2,19 +2,29 @@
 
 import scrapy
 
-class GadgetScraperItem(scrapy.Item):
-	# Main model for the scraper. To be exported in .csv
-	make = scrapy.Field()
-	model = scrapy.Field()
-	working_price_1 = scrapy.Field()
-	working_price_2 = scrapy.Field()
-	working_price_3 = scrapy.Field()
-	working_price_4 = scrapy.Field()
-	working_price_5 = scrapy.Field()
-	working_price_6 = scrapy.Field()
-	broken_price_1 = scrapy.Field()
-	broken_price_2 = scrapy.Field()
-	broken_price_3 = scrapy.Field()
-	broken_price_4 = scrapy.Field()
-	broken_price_5 = scrapy.Field()
-	broken_price_6 = scrapy.Field()
+
+class CompareMyMobileItem(scrapy.Item):
+    # Model for comparemymobile.com spiders
+    make = scrapy.Field()
+    model = scrapy.Field()
+    working_price_1 = scrapy.Field()
+    working_price_2 = scrapy.Field()
+    working_price_3 = scrapy.Field()
+    working_price_4 = scrapy.Field()
+    working_price_5 = scrapy.Field()
+    working_price_6 = scrapy.Field()
+    broken_price_1 = scrapy.Field()
+    broken_price_2 = scrapy.Field()
+    broken_price_3 = scrapy.Field()
+    broken_price_4 = scrapy.Field()
+    broken_price_5 = scrapy.Field()
+    broken_price_6 = scrapy.Field()
+
+
+class MusicMagpieItem(scrapy.Item):
+    # Model for musicmagpie.co.uk spiders
+    make = scrapy.Field()
+    model = scrapy.Field()
+    working_price_1 = scrapy.Field()
+    poor_condition_price_1 = scrapy.Field()
+    faulty_price_1 = scrapy.Field()
