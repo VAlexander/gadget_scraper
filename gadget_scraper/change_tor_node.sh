6814527f093b7244940ace555ac34964d08c9fab
Danil Bokov <a35012a@gmail.com>
2016-04-12 09:39:36 +0600

    Added management scripts

---
+++ gadget_scraper/change_tor_node.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+#Getting new TOR exit node
+
+empty -f -i torin -o torout telnet 127.0.0.1 9051
+empty -s -o torin "AUTHENTICATE \"gadget_scraper\"\n"
+empty -s -o torin "signal NEWNYM\n"
+empty -s -o torin "quit\n"
---
+++ gadget_scraper/run_monthly_crawling.sh
@@ -0,0 +1,2 @@
+cd /home/ubuntu/gadget_scraper/gadget_scraper
+python ./run_monthly_crawling.py
