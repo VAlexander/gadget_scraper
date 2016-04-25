0192dfc5c3b0d549733035fc4a4583b8eebe59d2
Danil Bokov <a35012a@gmail.com>
2016-04-25 17:47:30 +0600

    Fixed start method

--- gadget_scraper/run_monthly_crawling.sh
+++ gadget_scraper/run_monthly_crawling.sh
@@ -1,5 +1,6 @@
 cd /home/ubuntu/gadget_scraper/gadget_scraper
-python ./run_musicmagpie_crawling.py
+python ./run_musicmagpie_consoles_spider.py
+python ./run_musicmagpie_ipods_spider.py
 ./change_tor_node.sh
 echo "Changed TOR node"
 python ./run_compare_my_mobile_consoles_spider.py
